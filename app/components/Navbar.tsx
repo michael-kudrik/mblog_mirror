b0283affe15b5eddf955dab7a1f1a13e8169e267
Mike <mikethesurfer2@gmail.com>
2024-10-16 20:17:30 -0400

    edits

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
-import { FaYoutube, FaGithub, FaInstagram, FaLinkedin, FaHouseUser, } from "react-icons/fa";
+import {
+  FaYoutube,
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+  FaHouseUser,
+} from "react-icons/fa";
 
 export default function Navbar() {
   return (
-    <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
+    <nav className="!bg-pink-500 p-4 sticky top-0 drop-shadow-xl z-10">
       <div className="md:px-6 prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
         <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
           <Link
@@ -14,21 +20,40 @@ export default function Navbar() {
           </Link>
         </h1>
         <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
-            <Link className="text-white/90 hover:text-white" href="https://www.linkedin.com/in/michael-kudrik" target={"_blank"}>
-                <FaLinkedin></FaLinkedin>
-            </Link>
-            <Link className="text-white/90 hover:text-white" href="https://www.instagram.com/michael_kudrik/" target={"_blank"}>
-                <FaInstagram></FaInstagram>
-            </Link>
-            <Link className="text-white/90 hover:text-white" href="https://github.com/michael-kudrik" target={"_blank"}>
-                <FaGithub></FaGithub>
-            </Link> 
-            <Link className="text-white/90 hover:text-white" href="https://www.youtube.com/channel/UC5lVFEiOFT6QR9sJWy5Cmkg" target={"_blank"}>
-                <FaYoutube></FaYoutube>
-            </Link>
-            <Link className="text-white/90 hover:text-white" href="https://mikekudrik.boats/">
-                <FaHouseUser></FaHouseUser>
-            </Link>
+          <Link
+            className="text-white/90 hover:text-white"
+            href="https://www.linkedin.com/in/michael-kudrik"
+            target={"_blank"}
+          >
+            <FaLinkedin />
+          </Link>
+          <Link
+            className="text-white/90 hover:text-white"
+            href="https://www.instagram.com/michael_kudrik/"
+            target={"_blank"}
+          >
+            <FaInstagram />
+          </Link>
+          <Link
+            className="text-white/90 hover:text-white"
+            href="https://github.com/michael-kudrik"
+            target={"_blank"}
+          >
+            <FaGithub />
+          </Link>
+          <Link
+            className="text-white/90 hover:text-white"
+            href="https://www.youtube.com/channel/UC5lVFEiOFT6QR9sJWy5Cmkg"
+            target={"_blank"}
+          >
+            <FaYoutube />
+          </Link>
+          <Link
+            className="text-white/90 hover:text-white"
+            href="https://mikekudrik.boats/"
+          >
+            <FaHouseUser />
+          </Link>
         </div>
       </div>
     </nav>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-
+import {IBM_Plex_Sans} from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 
@@ -8,13 +8,19 @@ export const metadata: Metadata = {
   description: "Created by Mike Kudrik",
 };
 
+const ibmPlexSans = IBM_Plex_Sans({
+  weight: ['400', '700'],  // Specify the weights you need
+  display: 'swap',  // Specify the weights you need
+  subsets: ['latin'],             // Optional: Specify the character subsets
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={ibmPlexSans.className}>
       <body className="dark:bg-slate-800">
         <Navbar />
         <main className="px-4 md:=x-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
